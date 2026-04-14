103134e0283a16a475335a50f110e7850e2b6d90
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-04-15 03:56:21 +0530

    Feat: Products model created

---
+++ src/main/java/com/example/webdemo/Product.java
@@ -0,0 +1,8 @@
+package com.example.webdemo;
+
+public class Product {
+
+    private int proId;
+    private String proName;
+    private String price;
+}
---
+++ src/main/java/com/example/webdemo/ProductController.java
@@ -0,0 +1,7 @@
+package com.example.webdemo;
+
+public class ProductController {
+    public String getProducts() {
+        return "Products";
+    }
+}
