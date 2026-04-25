ca13269d3d090b0d2d60de612b34b2c8dfe9ef85
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-04-25 20:51:19 +0530

    Feat: POST Request

--- src/main/java/com/example/webdemo/controller/ProductController.java
+++ src/main/java/com/example/webdemo/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.example.webdemo.controller;
 import com.example.webdemo.model.Product;
 import com.example.webdemo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -16,13 +14,18 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts() {
         return service.getProducts();
     }
 
-    @RequestMapping("/products/{prodId}")
+    @GetMapping("/products/{prodId}")
     public Product getProductById(@PathVariable int prodId) {
         return  service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product) {
+        service.addProduct(product);
     }
 }
--- src/main/java/com/example/webdemo/service/ProductService.java
+++ src/main/java/com/example/webdemo/service/ProductService.java
@@ -1,25 +1,30 @@
 package com.example.webdemo.service;
 
 import com.example.webdemo.model.Product;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>( Arrays.asList(
             new Product(101,"Iphone",30000),
             new Product(102,"Camera", 34400)
-    );
-    public List<Product> getProducts() {
-        return products;
-    }
+    ));
 
     public Product getProductById(int prodId) {
         return products.stream()
                 .filter(p -> p.getProId() == prodId)
                 .findFirst().orElse(new Product(100,"No Item",0));
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
     }
 }
